7b10a26f42d0f7603c78c99afa181f3def316163
yonas39 <124627215+yonas39@users.noreply.github.com>
2023-02-10 18:14:27 -0500

    completed instruction 1-4
    1. Basic HTML
    3. Connecting the list of tasks to our HTML list
    4. Hooked the add and delete button
       -in the index.js three functions are provided namely addItem(), deleteItem(i) and clearCompleted(). using the @click action method I make the add and delete buttons functional.

--- index.js
+++ index.js
@@ -1,46 +1,48 @@
 import { createApp, Vue } from "https://mavue.mavo.io/mavue.js";
 
-let app = createApp({
-	data: {
-		tasks: [
-			{
-				done: false,
-				title: "",
-				active: true,
-			}
-		]
-	},
+let app = createApp(
+  {
+    data: {
+      tasks: [
+        {
+          done: false,
+          title: "",
+          active: true,
+        },
+      ],
+    },
 
-	methods: {
-		addItem() {
-			this.tasks.push({});
+    methods: {
+      addItem() {
+        this.tasks.push({});
 
-			this.setActive();
-		},
+        this.setActive();
+      },
 
-		deleteItem(i) {
-			this.tasks.splice(i, 1);
-			this.setActive(i - 1);
-		},
+      deleteItem(i) {
+        this.tasks.splice(i, 1);
+        this.setActive(i - 1);
+      },
 
-		clearCompleted () {
-			this.tasks = this.tasks.filter(task => !task.done);
-		},
+      clearCompleted() {
+        this.tasks = this.tasks.filter((task) => !task.done);
+      },
 
-		setActive(i) {
-			for (let task of this.tasks) {
-				task.active = false;
-			}
+      setActive(i) {
+        for (let task of this.tasks) {
+          task.active = false;
+        }
 
-			if (i >= 0 && this.tasks[i]) {
-				this.tasks[i].active = true;
-			}
-			else if (this.tasks.length > 0) {
-				this.tasks.at(-1).active = true;
-			}
-		},
-	}
-}, "#app");
+        if (i >= 0 && this.tasks[i]) {
+          this.tasks[i].active = true;
+        } else if (this.tasks.length > 0) {
+          this.tasks.at(-1).active = true;
+        }
+      },
+    },
+  },
+  "#app"
+);
 
 // For debugging
 globalThis.app = app;
